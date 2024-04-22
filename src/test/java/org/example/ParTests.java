84b44451ae710c7c0203636df5e2fd739c001553
NARASIMHA\YOGASHANKAR <yogashankar.ps@outlook.com>
2024-04-22 20:14:27 +0530

    logs added and logger changed to log to file

--- src/test/java/org/example/ParTests.java
+++ src/test/java/org/example/ParTests.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
@@ -10,33 +12,41 @@ import org.testng.annotations.*;
 import java.sql.Driver;
 
 public class ParTests {
+    protected static final Logger logger = LogManager.getLogger();
     ThreadLocal<WebDriver> driver = new ThreadLocal<>();
     @BeforeMethod
     public void initiate(ITestContext ctx){
+        logger.debug("started beforemethod hook");
         driver.set(DriverManager.getDriver());
         ctx.setAttribute("driver", driver.get());
+        logger.debug("ended before method hook");
     }
 
     @AfterClass
     public void teardown(){
         driver.get().quit();
+        logger.debug("called driver.quit");
     }
 
     @Test
     public void method_one(ITestContext ctx){
+        logger.info("started method_one");
         HomePage hp = new HomePage((WebDriver) ctx.getAttribute("driver"));
         hp.getHomrPage();
         hp.enterSearchText("love");
 
         int c = hp.getSearchResCount("love");
         Assert.assertEquals(c,1);
+        logger.info("ended method_one");
     }
     @Test
     public void method_two(ITestContext ctx){
+        logger.info("started method_two");
         HomePage hp = new HomePage((WebDriver) ctx.getAttribute("driver"));
         hp.getHomrPage();
         hp.enterSearchText("goal");
         int c = hp.getSearchResCount("goal");
         Assert.assertEquals(c,1);
+        logger.info("ended method_two");
     }
 }
--- src/test/java/org/example/ParTests2.java
+++ src/test/java/org/example/ParTests2.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.ITestContext;
@@ -8,27 +10,34 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class ParTests2 {
+    protected static final Logger logger = LogManager.getLogger();
     ThreadLocal<WebDriver> driver = new ThreadLocal<>();
     @BeforeMethod
     public void initiate(ITestContext ctx){
+        logger.debug("Started beforeMethod hook execution");
         driver.set(DriverManager.getDriver());
         ctx.setAttribute("driver", driver.get());
+        logger.debug("ended before method execution");
     }
 
     @Test
     public void method_one(ITestContext ctx){
+        logger.info("started Method_one test");
         HomePage hp = new HomePage((WebDriver) ctx.getAttribute("driver"));
         hp.getHomrPage();
         hp.enterSearchText("love");
         int c = hp.getSearchResCount("love");
         Assert.assertEquals(c,1);
+        logger.info("ended method_one test");
     }
     @Test
     public void method_two(ITestContext ctx){
+        logger.info("started method_two test");
         HomePage hp = new HomePage((WebDriver) ctx.getAttribute("driver"));
         hp.getHomrPage();
         hp.enterSearchText("goal");
         int c = hp.getSearchResCount("goal");
         Assert.assertEquals(c,1);
+        logger.info("ended method_two test");
     }
 }
